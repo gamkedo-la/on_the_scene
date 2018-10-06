0a4571a1b67574b6751a7aaadae0afa5e5510b49
Jeremy Jackson <jeremy.jackson089@gmail.com>
2018-10-05 22:27:33 -0400

    Mission node UI started

--- Assets/Scripts/MissionNode.cs
+++ Assets/Scripts/MissionNode.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MissionNode : MonoBehaviour {
 	public MissionType type;
@@ -11,10 +12,17 @@ public class MissionNode : MonoBehaviour {
 	private bool missionAccepted;
 	private bool canAcceptMission;
 	private bool missionComplete;
-    private MeshRenderer meshRenderer;
+	private MeshRenderer meshRenderer;
+	private GameObject missionAcceptPanel;
+	private Text missionAcceptTitle;
+	private Text missionAcceptDescription;
 
-    void Start () {
+	void Start () {
 		meshRenderer = gameObject.GetComponent<MeshRenderer>();
+		missionAcceptPanel = GameObject.Find("MissionAcceptPanel");
+		missionAcceptTitle = GameObject.Find("MissionTitle").GetComponent<Text>();
+		missionAcceptDescription = GameObject.Find("MissionDescription").GetComponent<Text>();
+		missionAcceptPanel.SetActive(false);
 	}
 
 	// Update is called once per frame
@@ -23,6 +31,7 @@ public class MissionNode : MonoBehaviour {
 		if (canAcceptMission && !missionAccepted && playerHasAcceptedMission) {
 			missionAccepted = true;
 			meshRenderer.enabled = false;
+			missionAcceptPanel.SetActive(false);
 			Debug.Log("Player has accepted the mission!");
 			Debug.Log("MISSION: " + missionTitle);
 			Debug.Log("GOAL: " + missionDescription);
@@ -33,14 +42,18 @@ public class MissionNode : MonoBehaviour {
 		if (other.gameObject.CompareTag("Player")) {
 			Debug.Log("Player touched the node!");
 			canAcceptMission = true;
+			missionAcceptPanel.SetActive(true);
+			missionAcceptTitle.text = missionTitle;
+			missionAcceptDescription.text = missionDescription;
 		}
 	}
 
 	void OnTriggerExit(Collider other) {
-        if (other.gameObject.CompareTag("Player")) {
-            Debug.Log("Player stopped touching the node!");
-            canAcceptMission = false;
-        }
+		if (other.gameObject.CompareTag("Player")) {
+			Debug.Log("Player stopped touching the node!");
+			canAcceptMission = false;
+			missionAcceptPanel.SetActive(false);
+		}
 	}
 }
 
