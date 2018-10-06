2897527443e7c0b803bb955036c85cfeeb3668b4
OmegaLarmor <remy-lapointe@hotmail.ca>
2018-10-05 23:05:15 -0400

    Toying with a NavMeshAgent version of the car mover

---
+++ Assets/Scripts/CarMoverNavMesh.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarMoverNavMesh : MonoBehaviour {
+
+	[SerializeField]
+	WayPointData _destination;
+
+	NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start ()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		SetDestination();
+	}
+	
+	void SetDestination () {
+
+		if (_destination != null) {
+			Vector3 targetVector = _destination.transform.position;
+			navMeshAgent.SetDestination(targetVector);
+		}
+	}
+
+	void Update () {
+
+		if (navMeshAgent.remainingDistance == 0) {
+			NextWaypoint();
+		}
+	}
+
+	void NextWaypoint() {
+
+		_destination = _destination.SelectRandomWaypoint();
+		navMeshAgent.SetDestination(_destination.transform.position);
+	}
+}
