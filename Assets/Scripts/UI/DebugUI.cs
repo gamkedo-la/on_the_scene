92bb38c4c856e36d10595b1af5a24ee540b1708c
Jeremy Kenyon <lanthos@gmail.com>
2018-10-21 12:37:27 -1000

    DebugUI singleton for adding debug info to the screen in realtime without clogging up the logs

---
+++ Assets/Scripts/UI/DebugUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugUI : MonoBehaviour {
+
+    public static DebugUI instance;
+    private Text debugOutput;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        instance = this;
+        debugOutput = transform.GetComponentInChildren<Text>();
+    }
+
+    public void SetDebugText(string textToSet) {
+        debugOutput.text = textToSet;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
