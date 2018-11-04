e20f0fffe9870ea685ad3e48249e75e9e68a402a
Jeremy Kenyon <lanthos@gmail.com>
2018-11-04 13:10:26 -1000

    created main menu and moved rescue heli prefab into the construction scene.

---
+++ Assets/Scripts/MenuHelicopterController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHelicopterController : MonoBehaviour {
+    private Quaternion startRot;
+    private void Start()
+    {
+        startRot = transform.localRotation;
+    }
+    // Update is called once per frame
+    void Update () {
+        transform.rotation = startRot * Quaternion.AngleAxis(3.0f * Mathf.Cos(0.5f * Time.timeSinceLevelLoad), Vector3.right) *
+            Quaternion.AngleAxis(2.0f * Mathf.Cos(0.7f * Time.timeSinceLevelLoad), Vector3.forward);
+
+    }
+}
