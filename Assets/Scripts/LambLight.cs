68194244efdfe127d5ae378a5aff119ba4cca792
bariskoklu <bariskoklu@gmail.com>
2018-11-08 12:45:57 +0300

    Lamb light gets activated on night

---
+++ Assets/Scripts/LambLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LambLight : MonoBehaviour {
+
+    // Use this for initialization
+    private Transform lightCone;
+
+	void Start () {
+        lightCone =  transform.Find("LightCone");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!WorldState.instance.isDay)
+        {
+            lightCone.gameObject.SetActive(true);
+        }
+        else
+        {
+            lightCone.gameObject.SetActive(false);
+        }
+	}
+}
