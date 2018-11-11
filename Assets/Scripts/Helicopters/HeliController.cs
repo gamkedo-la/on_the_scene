fa9e0290497b18677646366e2ab9f5ec7f473773
KornelGM <KORNik_k@wp.pl>
2018-11-11 18:58:34 +0100

    Test scene renamed. Heli "snappiness" improved. Code refactord for future modifications.

--- Assets/Scripts/Helicopters/HeliController.cs
+++ Assets/Scripts/Helicopters/HeliController.cs
@@ -1,13 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Assertions;
 
-enum ConnectedController
-{
-	None,
-	XBox,
-	PS4orOther,
-}
-
 public class HeliController : MonoBehaviour
 {
 	[HideInInspector] public static HeliController instance;
@@ -28,38 +21,10 @@ public class HeliController : MonoBehaviour
 	[SerializeField] private AnimationCurve dragCurve = new AnimationCurve( new Keyframe( 0f, 0f ), new Keyframe( 1f, 0.1f ) );
 	[SerializeField] private AnimationCurve liftCurve = new AnimationCurve( new Keyframe( 0f, 1f ), new Keyframe( 1f, 0f ) );
 
-	[Header("Throttle")]
-	[SerializeField] private float throttleChangeSpeed = 3f;
-	[SerializeField] private float throttleBackChangeSpeed = 6f;
 	[SerializeField] private float throttleHitDampning = 3f;
-	[SerializeField] private float maxThrottle = 10f;
 
-	[Header("Pitch")]
-	[SerializeField] private float pitchDesiredChangeSpeed = 100f;
-	[SerializeField] private float pitchChangeSpeed = 0.3f;
-	[SerializeField] private float maxPitch = 45f;
-
-	[Header("Roll")]
-	[SerializeField] private float rollDesiredChangeSpeed = 100f;
-	[SerializeField] private float rollChangeSpeed = 0.3f;
-	[SerializeField] private float maxRoll = 45f;
-
-	[Header("Yaw")]
-	[SerializeField] private float yawChangeSpeed = 30f;
-
-	[Header("Other")]
-	[SerializeField] private float deadZone = 10f;
-
-	private float currentThrottle = 0;
-	private float currentYaw = 0;
-	private float currentPitch = 0;
-	private float currentPitchDesired = 0;
-	private float currentRoll = 0;
-	private float currentRollDesired = 0;
+	private HeliInput input = null;
 	private float currentVelocity = 0;
-	private float rollControllerLast = 0;
-	private float pitchControllerLast = 0;
-	private ConnectedController connectedController = ConnectedController.None;
 	private Vector3 lastPosition;
 
 	void Start( )
@@ -76,6 +41,9 @@ public class HeliController : MonoBehaviour
 
 	void Awake( )
 	{
+		input = GetComponent<HeliInput>( );
+		Assert.IsNotNull( input );
+
 		TakeInstance( );
 	}
 
@@ -89,42 +57,36 @@ public class HeliController : MonoBehaviour
 		Cursor.lockState = CursorLockMode.None;
 	}
 
-	void Update( )
-	{
-		HandleControls( );
-	}
-
 	void FixedUpdate( )
 	{
-		CheckConnectedControllers( ); // Or do we only check on start?
 		RotateAndMove( );
 	}
 
 	void OnCollisionStay( Collision collision )
 	{
-		if ( currentThrottle > 0 )
+		if ( input.currentThrottle > 0 )
 		{
-			currentThrottle -= throttleHitDampning * 2 * Time.deltaTime;
-			currentThrottle = currentThrottle < 0 ? 0 : currentThrottle;
+			input.currentThrottle -= throttleHitDampning * 2 * Time.deltaTime;
+			input.currentThrottle = input.currentThrottle < 0 ? 0 : input.currentThrottle;
 		}
 		else
 		{
-			currentThrottle += throttleHitDampning * 2 * Time.deltaTime;
-			currentThrottle = currentThrottle > 0 ? 0 : currentThrottle;
+			input.currentThrottle += throttleHitDampning * 2 * Time.deltaTime;
+			input.currentThrottle = input.currentThrottle > 0 ? 0 : input.currentThrottle;
 		}
 	}
 
 	void OnCollisionEnter( Collision collision )
 	{
-		if (currentThrottle > 0)
+		if ( input.currentThrottle > 0)
 		{
-			currentThrottle -= throttleHitDampning;
-			currentThrottle = currentThrottle < 0 ? 0 : currentThrottle;
+			input.currentThrottle -= throttleHitDampning;
+			input.currentThrottle = input.currentThrottle < 0 ? 0 : input.currentThrottle;
 		}
 		else
 		{
-			currentThrottle += throttleHitDampning;
-			currentThrottle = currentThrottle > 0 ? 0 : currentThrottle;
+			input.currentThrottle += throttleHitDampning;
+			input.currentThrottle = input.currentThrottle > 0 ? 0 : input.currentThrottle;
 		}
 	}
 
@@ -133,29 +95,29 @@ public class HeliController : MonoBehaviour
 		instance = this;
 	}
 
-	public float GetPitchPercent()
+	public float GetPitchPercent( )
 	{
-		return currentPitch / maxPitch;
+		return input.currentPitch / input.maxPitch;
 	}
 
 	public float GetRollPercent( )
 	{
-		return currentRoll / maxRoll;
+		return input.currentRoll / input.maxRoll;
 	}
 
 	public float GetPitchDesieredPercent( )
 	{
-		return currentPitchDesired / maxPitch;
+		return input.currentPitchDesired / input.maxPitch;
 	}
 
 	public float GetRollDesieredPercent( )
 	{
-		return currentRollDesired / maxRoll;
+		return input.currentRollDesired / input.maxRoll;
 	}
 
 	public float GetThrottlePercent( )
 	{
-		return currentThrottle / maxThrottle;
+		return input.currentThrottle / input.maxThrottle;
 	}
 
 	public float GetVelocity( )
@@ -173,148 +135,6 @@ public class HeliController : MonoBehaviour
 		return worldScale;
 	}
 
-	private void CheckConnectedControllers()
-	{
-		string[] controllers = Input.GetJoystickNames( );
-
-		connectedController = ConnectedController.None;
-		for ( int i = 0; i < controllers.Length; i++ ) // Is there at least one?
-		{
-			if ( controllers[i].Length > 0)
-				connectedController = ConnectedController.PS4orOther; // We got a controller (PS4 or other)
-
-			if ( controllers[i].ToLower( ).Contains( "xbox" ) ) // Is it an XBox controller?
-			{
-				connectedController = ConnectedController.XBox;
-				return;
-			}
-		}
-	}
-
-	private void HandleControls( )
-	{
-		// Throttle
-		float throttleController = -Input.GetAxis( "Throttle" );
-		if (throttleController != 0 )
-		{
-			currentThrottle += throttleController * throttleChangeSpeed * Time.deltaTime;
-			currentThrottle = Mathf.Clamp
-			(
-				currentThrottle,
-				-maxThrottle * Mathf.Abs( throttleController ),
-				maxThrottle * Mathf.Abs( throttleController )
-			);
-		}
-
-		float throttleKeyboard = Input.GetAxis( "Vertical" );
-		if ( throttleKeyboard != 0 )
-		{
-			currentThrottle += throttleKeyboard * throttleChangeSpeed * Time.deltaTime;
-		}
-		else if ( throttleController == 0 )
-		{
-			if ( currentThrottle > 0.1f )
-				currentThrottle -= throttleBackChangeSpeed * Time.deltaTime;
-			else if ( currentThrottle < -0.1f )
-				currentThrottle += throttleBackChangeSpeed * Time.deltaTime;
-			else
-				currentThrottle = 0;
-		}
-
-		currentThrottle = Mathf.Clamp( currentThrottle, -maxThrottle, maxThrottle );
-
-		// Roll
-		float rollController = 0;
-
-		if ( connectedController == ConnectedController.XBox )
-        {
-            rollController = -Input.GetAxis("Roll");
-        }
-        else
-        {
-            rollController = -Input.GetAxis("RollPS4");
-        }
-
-        if ( Input.GetAxis( "Mouse X" ) > 0.1f )
-        {
-            currentRollDesired -= rollDesiredChangeSpeed * Time.deltaTime;
-        }
-        else if ( Input.GetAxis( "Mouse X" ) < -0.1f )
-        {
-            currentRollDesired += rollDesiredChangeSpeed * Time.deltaTime;
-        }
-		else if ( rollController != 0 )
-        {
-            currentRollDesired = rollController * maxRoll;
-        }
-		else if ( rollController == 0 && rollController != rollControllerLast )
-        {
-            currentRollDesired = 0;
-        }
-
-		currentRollDesired = Mathf.Clamp( currentRollDesired, -maxRoll, maxRoll );
-		float target = currentRollDesired < deadZone && currentRollDesired > -deadZone ? 0 : currentRollDesired; // Dead Zone
-		currentRoll = Mathf.Lerp( currentRoll, target, rollChangeSpeed * Time.deltaTime );
-		rollControllerLast = rollController;
-
-		// Pitch
-		float pitchController = 0;
-
-		if ( connectedController == ConnectedController.XBox )
-        {
-            pitchController = -Input.GetAxis("Pitch");
-        }
-        else {
-            pitchController = -Input.GetAxis("PitchPS4");
-        }
-
-        if ( Input.GetAxis( "Mouse Y" ) > 0.1f )
-        {
-            currentPitchDesired += pitchDesiredChangeSpeed * Time.deltaTime;
-        }
-        else if ( Input.GetAxis( "Mouse Y" ) < -0.1f )
-        {
-            currentPitchDesired -= pitchDesiredChangeSpeed * Time.deltaTime;
-        }
-		else if ( pitchController != 0 )
-        {
-            currentPitchDesired = pitchController * maxPitch;
-        }
-		else if ( pitchController == 0 && pitchController != pitchControllerLast )
-        {
-            currentPitchDesired = 0;
-        }
-
-		currentPitchDesired = Mathf.Clamp( currentPitchDesired, -maxPitch-deadZone, maxPitch+deadZone );
-		target = currentPitchDesired < deadZone && currentPitchDesired > -deadZone ? 0 : currentPitchDesired; // Dead Zone
-		currentPitch = Mathf.Lerp( currentPitch, target, pitchChangeSpeed * Time.deltaTime );
-		pitchControllerLast = pitchController;
-
-		// Yaw
-		float yawL = 0;
-		float yawR = 0;
-
-		if ( connectedController == ConnectedController.XBox )
-		{
-			yawL = -Input.GetAxis( "YawLeft" );
-			yawR = Input.GetAxis( "YawRight" );
-		}
-		else
-		{
-			yawL = 0; // Jeremy, please add Yaw Input.GetAxis or Buttons for PS4
-			yawR = 0;
-		}
-
-		float yawController = yawL + yawR;
-		float yawKeyboard = Input.GetAxis( "Horizontal" );
-		float yaw = yawController + yawKeyboard;
-		yaw = Mathf.Clamp( yaw, -1, 1 );
-
-		currentYaw += yaw * yawChangeSpeed * Time.deltaTime;
-		currentYaw = currentYaw > 360 ? currentYaw - 360 : currentYaw; // So it's not bigger then 360*
-		currentYaw = currentYaw < -360 ? currentYaw + 360 : currentYaw; // So it's not smaller then -360*
-	}
-
 	private void RotateAndMove( )
 	{
 		// Velocity
@@ -322,12 +142,12 @@ public class HeliController : MonoBehaviour
 		lastPosition = transform.position;
 
 		// Heli rotation
-		heliRigidbody.MoveRotation( Quaternion.Euler( currentPitch, currentYaw, currentRoll ) );
-		float rollPercent = currentRoll / maxRoll;
-		float pitchPercent = currentPitch / maxPitch;
+		heliRigidbody.MoveRotation( Quaternion.Euler( input.currentPitch, input.currentYaw, input.currentRoll ) );
+		float rollPercent = input.currentRoll / input.maxRoll;
+		float pitchPercent = input.currentPitch / input.maxPitch;
 
 		// Rotor animations
-		float rotationControl = currentThrottle + maxThrottle + 5;
+		float rotationControl = input.currentThrottle + input.maxThrottle + 5;
 		rotationControl *= 90 * Time.fixedDeltaTime;
 		rotorMain.transform.Rotate( Vector3.up, rotationControl );
 		rotorTail.transform.Rotate( Vector3.right, rotationControl );
@@ -342,7 +162,7 @@ public class HeliController : MonoBehaviour
 		moveVector = Mathf.Abs( rollPercent ) > Mathf.Abs( pitchPercent ) ?
 			moveVector * Mathf.Abs( rollPercent ) : moveVector * Mathf.Abs( pitchPercent );
 
-		float horizontalThrottle = currentThrottle;
+		float horizontalThrottle = input.currentThrottle;
 		horizontalThrottle = horizontalThrottle < 0 ? 1 : horizontalThrottle; // Always give some speed
 		horizontalThrottle = horizontalThrottle >= 0 && horizontalThrottle < 1 ? 1 : horizontalThrottle;
 
@@ -352,22 +172,22 @@ public class HeliController : MonoBehaviour
 		moveVector = Quaternion.Euler( 0, heliRigidbody.rotation.eulerAngles.y, 0 ) * moveVector;
 
 		// Ascend / descend
-		if ( currentThrottle >= 0 )
+		if ( input.currentThrottle >= 0 )
 		{
 			// How much "speed" is used for horizontal movement?
 			float horizontalPercent = Mathf.Abs( rollPercent ) >= Mathf.Abs( pitchPercent ) ?
 				Mathf.Abs( rollPercent ) : Mathf.Abs( pitchPercent );
 
 			// How much each "force" contributes to vertical movement?
-			float downDrag = -maxThrottle * dragCurve.Evaluate( horizontalPercent );
-			float throttle = currentThrottle * liftCurve.Evaluate( horizontalPercent );
+			float downDrag = -input.maxThrottle * dragCurve.Evaluate( horizontalPercent );
+			float throttle = input.currentThrottle * liftCurve.Evaluate( horizontalPercent );
 
 			// Final vertical movement
 			moveVector.y = ( downDrag + throttle ) * ( speedMax / worldScale ) * Time.fixedDeltaTime;
 		}
 		else
 		{
-			moveVector.y = currentThrottle * ( speedMax / worldScale ) * Time.fixedDeltaTime;
+			moveVector.y = input.currentThrottle * ( speedMax / worldScale ) * Time.fixedDeltaTime;
 		}
 
 		heliRigidbody.MovePosition( heliRigidbody.position + moveVector );
