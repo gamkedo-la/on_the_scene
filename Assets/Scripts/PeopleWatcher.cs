efc7b437e762efc4961b84e925802b998607f2da
Jeremy Kenyon <lanthos@gmail.com>
2018-11-18 16:01:17 -1000

    Organized some things into folders, created another island, added more building textures, added little people from Divine Disaster (please someone, provider better ones)

---
+++ Assets/Scripts/PeopleWatcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleWatcher : MonoBehaviour {
+
+    private Transform cameraTransform;
+	// Use this for initialization
+	void Start () {
+        cameraTransform = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 flatPt = cameraTransform.position;
+        flatPt.y = transform.position.y;
+        transform.LookAt(flatPt);
+        transform.Rotate(Vector3.up, 180.0f);
+	}
+}
