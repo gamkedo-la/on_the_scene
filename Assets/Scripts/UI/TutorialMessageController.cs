2d95abd8d4cbfccdd56ea20556ab7d89bf882e26
KornelGM <KORNik_k@wp.pl>
2018-11-21 16:15:45 +0100

    Added throttle tutorial message

---
+++ Assets/Scripts/UI/TutorialMessageController.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class TutorialMessageController : MonoBehaviour
+{
+	[SerializeField] private TextMeshProUGUI textToShow = null;
+
+	void Start ()
+	{
+		Assert.IsNotNull( textToShow );
+	}
+
+	private void OnTriggerEnter( Collider other )
+	{
+		if ( !other.CompareTag( "Player" ) )
+			return;
+
+		textToShow.gameObject.SetActive( true );
+	}
+
+	private void OnTriggerExit( Collider other )
+	{
+		if ( !other.CompareTag( "Player" ) )
+			return;
+
+		textToShow.gameObject.SetActive( false );
+	}
+}
