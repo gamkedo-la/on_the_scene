ab70e1c8c3ec37ffd86601a5a4450c506293897f
Terrence <t.w.mcdonnell10@gmail.com>
2018-12-12 16:55:33 -0700

    Script to check if heli is close to tree (beginnings of swaying script)

---
+++ Assets/Scripts/SwayWhenHelicopterIsNear.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwayWhenHelicopterIsNear : MonoBehaviour {
+    public Transform helicopter;
+	
+	void Start () {
+       
+    }
+	
+	void Update () {
+        if (Vector3.Distance(helicopter.position, transform.position) < 10.0f) {
+            Debug.Log("close enough");
+        }
+    }
+}
