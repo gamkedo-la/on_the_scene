89e1a721900fc99caaeb7f537472c5eec4889654
Terrence <t.w.mcdonnell10@gmail.com>
2018-12-14 15:41:45 -0700

    Sway Script Update (WIP)

--- Assets/Scripts/SwayWhenHelicopterIsNear.cs
+++ Assets/Scripts/SwayWhenHelicopterIsNear.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwayWhenHelicopterIsNear : MonoBehaviour {
-    public Transform helicopter;
-	
-	void Start () {
-       
+    public Quaternion originalRotation;
+    float rotationResetSpeed = 1.0f;
+    float distanceCheck = 10.0f;
+
+    void Start () {
+        originalRotation = transform.rotation;
     }
 	
 	void Update () {
-        if (Vector3.Distance(helicopter.position, transform.position) < 10.0f) {
-            Debug.Log("close enough");
+        var treeWobbleScale = Vector3.Distance(HeliController.instance.transform.position, transform.position);
+        if (treeWobbleScale < distanceCheck) {
+            Debug.Log("swaying");
+            transform.rotation = Quaternion.FromToRotation(Vector3.up,
+                HeliController.instance.transform.forward * treeWobbleScale)
+                * Quaternion.AngleAxis(treeWobbleScale,
+                HeliController.instance.transform.forward);
+        } else {
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotation, 
+                Time.deltaTime * rotationResetSpeed);
         }
     }
 }
