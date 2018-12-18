e1f6634e5978d3c5ca7c139d486b1550c093c46e
KornelGM <KORNik_k@wp.pl>
2018-12-18 18:12:32 +0100

    The Heli can now start (as in we can turn on the engine)

---
+++ Assets/Scripts/Helicopters/HeliStartAndLanding.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliStartAndLanding : MonoBehaviour
+{
+	private enum EngineState
+	{
+		CanBeTurnedOn,
+		Running,
+		CanBeTurnedOff
+	}
+
+	[SerializeField] private Behaviour[] behavioursToChange = null;
+	[SerializeField] private GameObject[] gameObjectsToChange = null;
+	[SerializeField] private GameObject tutorialMessage = null;
+
+	private EngineState engineState = EngineState.CanBeTurnedOn;
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+		if ( engineState == EngineState.CanBeTurnedOn && Input.GetKeyDown( KeyCode.E ) )
+		{
+			engineState = EngineState.Running;
+			if ( tutorialMessage )
+				tutorialMessage.SetActive( false );
+			SetAll( true );
+		}
+	}
+
+	private void SetAll( bool state )
+	{
+		foreach ( var behaviour in behavioursToChange )
+			behaviour.enabled = state;
+
+		foreach ( var gameObj in gameObjectsToChange )
+			gameObj.SetActive( state );
+	}
+}
