04b9933b9441d7af95542b3c4a945060e941bcb6
Terrence <t.w.mcdonnell10@gmail.com>
2018-12-18 16:06:23 -0700

    Begin Dust Cloud pfx and script

---
+++ Assets/Scripts/ActivateDustClouds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDustClouds : MonoBehaviour {
+    ParticleSystem ps;
+    public Mesh ground;
+    float distanceFromGround = 3.0f;
+
+    // Use this for initialization
+    void Start () {
+        ps = gameObject.GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		////var distanceFromGround = Vector3.Distance(ground.position, transform.position);
+        //Debug.Log("Distance to other: " + distanceFromGround);
+        //if (distanceFromGround < distanceCheck) {
+        //    ps.Play();
+        //} else {
+        //    ps.Stop();
+        //}
+    }
+}
