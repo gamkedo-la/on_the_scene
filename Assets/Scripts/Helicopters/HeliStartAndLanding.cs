ab5971b29f8679f796738a39906770543b952c74
KornelGM <KORNik_k@wp.pl>
2018-12-30 12:46:05 +0100

    The Heli can be landed if low enough and speed is slow enough

--- Assets/Scripts/Helicopters/HeliStartAndLanding.cs
+++ Assets/Scripts/Helicopters/HeliStartAndLanding.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class HeliStartAndLanding : MonoBehaviour
 {
@@ -14,12 +15,19 @@ public class HeliStartAndLanding : MonoBehaviour
 	[SerializeField] private Behaviour[] behavioursToChange = null;
 	[SerializeField] private GameObject[] gameObjectsToChange = null;
 	[SerializeField] private GameObject tutorialMessage = null;
+	[SerializeField] private HeliController heli = null;
+	[SerializeField] private float minLandingVelocity = 1f;
+	[SerializeField] private float minLandingAltitude = 1f;
 
 	private EngineState engineState = EngineState.CanBeTurnedOn;
+	private RaycastHit hit;
+	private float worldScale = 1f;
 
 	void Start ()
 	{
+		Assert.IsNotNull( heli );
 
+		worldScale = heli.GetWorldScale( );
 	}
 
 	void Update ()
@@ -31,6 +39,13 @@ public class HeliStartAndLanding : MonoBehaviour
 				tutorialMessage.SetActive( false );
 			SetAll( true );
 		}
+		else if ( engineState == EngineState.Running && Input.GetKeyDown( KeyCode.E ) && HeliIsAlmostStill( ) )
+		{
+			engineState = EngineState.CanBeTurnedOn;
+			SetAll( false );
+			heli.transform.parent.GetComponent<Rigidbody>( ).velocity = Vector3.zero;
+			heli.transform.parent.GetComponent<Rigidbody>( ).angularVelocity = Vector3.zero;
+		}
 	}
 
 	private void SetAll( bool state )
@@ -40,5 +55,12 @@ public class HeliStartAndLanding : MonoBehaviour
 
 		foreach ( var gameObj in gameObjectsToChange )
 			gameObj.SetActive( state );
+	}
+
+	private bool HeliIsAlmostStill()
+	{
+		Physics.Raycast( heli.GetAltitudePoint().position, Vector3.down, out hit );
+
+		return heli.GetVelocity( ) <= minLandingVelocity && ( hit.distance * worldScale ) <= minLandingAltitude;
 	}
 }
