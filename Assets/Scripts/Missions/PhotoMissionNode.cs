832d8dc81bae6e02122db6e758dcac115ae3e696
Jeremy Kenyon <lanthos@gmail.com>
2018-12-29 20:19:19 -1000

    Reoganizing some files and creating a PhotoMissionNode script.

---
+++ Assets/Scripts/Missions/PhotoMissionNode.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoMissionNode : MonoBehaviour {
+
+    HeliController player;
+    public float counter = 0.0f;
+    public float maxTime = 20.0f;
+    public bool pictureTaken = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HeliController temp = other.GetComponent<HeliController>();
+        if (temp != null)
+        {
+            player = temp;
+            StartCoroutine(UpdateCounter());
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (player != null)
+        {
+            Debug.Log("Player is in collider and staying");
+            if (counter <= maxTime)
+            {
+                pictureTaken = true;
+                StopCoroutine(UpdateCounter());
+            }
+        }
+    }
+
+    IEnumerator UpdateCounter()
+    {
+        yield return new WaitForSeconds(1);
+        if (player.timeSinceLastMove <= 0.0f)
+        {
+            counter += 1.0f;
+        }
+        else
+        {
+            counter = 0.0f;
+        }
+    }
+
+}
