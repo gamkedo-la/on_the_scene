85a4dbe9e17ce52e9759d46189b1df52e6d578bd
cb <csbutton01@hotmail.com>
2019-01-03 11:14:55 -0800

    remove the terrain call on the FlightWander script

--- Assets/Scripts/AI/FlightWander.cs
+++ Assets/Scripts/AI/FlightWander.cs
@@ -14,17 +14,9 @@ public class FlightWander : MonoBehaviour {
     void Start()
     {
         desiredPosition = transform.position;
-        float terrainY = Terrain.activeTerrain.SampleHeight(transform.position);
-        if (terrainY > transform.position.y)
-        {
-            Debug.Log("I was under the terrain!");
-            desiredPosition.y = Mathf.Max(desiredPosition.y, (terrainY + 4.0f));
-            transform.position = desiredPosition;
-        }
     }
-	
 	// Update is called once per frame
 	void Update () {
-		//this.transform.Translate (Vector3.forward * flightSpeed * Time.deltaTime);
+		this.transform.Translate (Vector3.forward * flightSpeed * Time.deltaTime);
 	}
 }
