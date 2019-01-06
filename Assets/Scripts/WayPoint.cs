c60e7654c05d19d669fbb4a0c0693946fdc6317a
Jeremy Kenyon <lanthos@gmail.com>
2019-01-06 08:56:12 -1000

    Added new car to the scene, hooked up waypoints, got traffic moving along.

---
+++ Assets/Scripts/WayPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WayPoint : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        WaypointFollower car = other.gameObject.GetComponent<WaypointFollower>();
+        Debug.Log("Someone entered me! That some is: " + other.name);
+        if (car != null)
+        {
+            car.reachedDesitnation = true;
+        }
+    }
+}
