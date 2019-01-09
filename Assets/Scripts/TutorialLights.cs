8562cf0ecf200c260f70658e97771bbca2416819
KornelGM <KORNik_k@wp.pl>
2019-01-09 17:10:38 +0100

    Updated the look of the tutorial, added guiding lights.

---
+++ Assets/Scripts/TutorialLights.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialLights : MonoBehaviour
+{
+	public Light[] Lights = null;
+	public float changeTime = 2f;
+
+	private int lastIndex = -1;
+
+	void Start ()
+	{
+		Invoke( "ChangeLights", 0.1f );
+	}
+
+	private void ChangeLights()
+	{
+		lastIndex++;
+
+		if ( lastIndex >= Lights.Length )
+			lastIndex = 0;
+
+		foreach ( var light in Lights )
+		{
+			light.enabled = false;
+		}
+
+		Lights[lastIndex].enabled = true;
+
+		lastIndex++;
+
+		if ( lastIndex >= Lights.Length )
+			lastIndex = 0;
+
+		Lights[lastIndex].enabled = true;
+
+		Invoke( "ChangeLights", changeTime );
+	}
+}
