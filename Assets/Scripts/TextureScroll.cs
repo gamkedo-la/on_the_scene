08133abb3b3fd54e5f3dfed0de10f87a9873ff53
Terrence <t.w.mcdonnell10@gmail.com>
2019-01-17 18:14:31 -0700

    Add lava texture and script to scroll texture

---
+++ Assets/Scripts/TextureScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScroll : MonoBehaviour
+{
+
+    // Scroll main texture based on time
+    public float scrollSpeed = 0.02f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Vector2 textureOffset = new Vector2(Time.time * scrollSpeed, Time.time * -scrollSpeed);
+        rend.material.mainTextureOffset = textureOffset;
+    }
+}
