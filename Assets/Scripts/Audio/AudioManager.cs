9c99b803fae6928edcb519f534ca004551bdd50b
Jeremy Kenyon <lanthos@gmail.com>
2019-02-04 19:04:22 -1000

    Added new mixer groups for chopper sounds and notchopper sounds as we need to manage these differently.  The chopper sounds are drowning out everything else.  Added them to the new sound pause menu.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -9,6 +9,8 @@ public class AudioManager : MonoBehaviour
     private FMOD.Studio.Bus master;
     private FMOD.Studio.Bus music;
     private FMOD.Studio.Bus SFX;
+    private FMOD.Studio.Bus chopperSFX;
+    private FMOD.Studio.Bus notChopperSFX;
 
     // Use this for initialization
     void Start()
@@ -16,9 +18,13 @@ public class AudioManager : MonoBehaviour
         master = FMODUnity.RuntimeManager.GetBus("bus:/Master");
         music = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
         SFX = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
+        chopperSFX = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX/Chopper");
+        notChopperSFX = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX/NotChopper");
         master.setVolume(1.0f);
-        music.setVolume(0.2f);
-        SFX.setVolume(0.2f);
+        music.setVolume(0.5f);
+        SFX.setVolume(0.5f);
+        chopperSFX.setVolume(0.05f);
+        notChopperSFX.setVolume(1.0f);
     }
 
     public void SetVolume(Slider volume)
@@ -34,6 +40,16 @@ public class AudioManager : MonoBehaviour
     public void SetSFXVolume(Slider volume)
     {
         SFX.setVolume(volume.value);
+    }
+
+    public void SetChopperSFXVolume(Slider volume)
+    {
+        chopperSFX.setVolume(volume.value);
+    }
+
+    public void SetNotChopperSFXVolume(Slider volume)
+    {
+        notChopperSFX.setVolume(volume.value);
     }
 
     // Update is called once per frame
