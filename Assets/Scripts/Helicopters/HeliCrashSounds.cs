06ada25f7a7c7ab94bdaf3cdc3b043d9f1fc3c29
Jeremy Kenyon <lanthos@gmail.com>
2019-02-04 21:10:25 -1000

    converted water crash sounds to 2D and created script to play one if you move into the water collider.

---
+++ Assets/Scripts/Helicopters/HeliCrashSounds.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliCrashSounds : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string CrashOneEvent;
+    private FMOD.Studio.EventInstance crashOneSound;
+
+    [FMODUnity.EventRef]
+    public string CrashTwoEvent;
+    private FMOD.Studio.EventInstance crashTwoSound;
+
+    [FMODUnity.EventRef]
+    public string CrashThreeEvent;
+    private FMOD.Studio.EventInstance crashThreeSound;
+
+    private Rigidbody cachedRigidBody;
+    // Use this for initialization
+    void Start()
+    {
+        cachedRigidBody = HeliController.instance.GetComponentInParent<Rigidbody>();
+        if (cachedRigidBody == null)
+        {
+            Debug.Log("Unable to get rigidbody from HeliController");
+        }
+
+        crashOneSound = FMODUnity.RuntimeManager.CreateInstance(CrashOneEvent);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(crashOneSound, GetComponent<Transform>(), GetComponent<Rigidbody>());
+
+        crashTwoSound = FMODUnity.RuntimeManager.CreateInstance(CrashTwoEvent);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(crashTwoSound, GetComponent<Transform>(), GetComponent<Rigidbody>());
+
+        crashThreeSound = FMODUnity.RuntimeManager.CreateInstance(CrashThreeEvent);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(crashThreeSound, GetComponent<Transform>(), GetComponent<Rigidbody>());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HeliController tempHC = other.gameObject.GetComponentInChildren<HeliController>();
+        if (tempHC != null)
+        {
+            Debug.Log("heli entered meeee");
+            crashOneSound.start();
+        }
+    }
+}
--- Assets/Scripts/WayPoint.cs
+++ Assets/Scripts/WayPoint.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 
-public class WayPoint : MonoBehaviour {
+public class WayPoint : MonoBehaviour
+{
 
     private void OnTriggerEnter(Collider other)
     {
         WaypointFollower car = other.gameObject.GetComponent<WaypointFollower>();
-        Debug.Log("Someone entered me! That some is: " + other.name);
+        //Debug.Log("Someone entered me! That some is: " + other.name);
         if (car != null)
         {
             car.reachedDesitnation = true;
