a283baf6387da17119fa2c01402f53334c40fbe5
Jeremy Kenyon <lanthos@gmail.com>
2019-02-05 16:45:46 -1000

    All crash sounds converted to 2D.  Now when entering water will randomly play one of the water crash sounds.

--- Assets/Scripts/Helicopters/HeliCrashSounds.cs
+++ Assets/Scripts/Helicopters/HeliCrashSounds.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class HeliCrashSounds : MonoBehaviour
 {
+    private List<FMOD.Studio.EventInstance> sounds;
     [FMODUnity.EventRef]
     public string CrashOneEvent;
     private FMOD.Studio.EventInstance crashOneSound;
@@ -16,10 +17,12 @@ public class HeliCrashSounds : MonoBehaviour
     public string CrashThreeEvent;
     private FMOD.Studio.EventInstance crashThreeSound;
 
+    private int soundCount = 0;
     private Rigidbody cachedRigidBody;
     // Use this for initialization
     void Start()
     {
+        sounds = new List<FMOD.Studio.EventInstance>();
         cachedRigidBody = HeliController.instance.GetComponentInParent<Rigidbody>();
         if (cachedRigidBody == null)
         {
@@ -28,12 +31,13 @@ public class HeliCrashSounds : MonoBehaviour
 
         crashOneSound = FMODUnity.RuntimeManager.CreateInstance(CrashOneEvent);
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(crashOneSound, GetComponent<Transform>(), GetComponent<Rigidbody>());
-
+        sounds.Add(crashOneSound);
         crashTwoSound = FMODUnity.RuntimeManager.CreateInstance(CrashTwoEvent);
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(crashTwoSound, GetComponent<Transform>(), GetComponent<Rigidbody>());
-
+        sounds.Add(crashTwoSound);
         crashThreeSound = FMODUnity.RuntimeManager.CreateInstance(CrashThreeEvent);
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(crashThreeSound, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        sounds.Add(crashThreeSound);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -41,8 +45,19 @@ public class HeliCrashSounds : MonoBehaviour
         HeliController tempHC = other.gameObject.GetComponentInChildren<HeliController>();
         if (tempHC != null)
         {
-            Debug.Log("heli entered meeee");
-            crashOneSound.start();
+            FMOD.Studio.EventInstance randomSound = SoundPicker();
+            randomSound.start();
         }
+    }
+
+    private FMOD.Studio.EventInstance SoundPicker()
+    {
+        if (soundCount >= sounds.Count)
+        {
+            soundCount = 0;
+        }
+        int tempCounter = soundCount;
+        soundCount += 1;
+        return sounds[tempCounter];
     }
 }
