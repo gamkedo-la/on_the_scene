5df3f0d0e85388f6da2fab21fef502fdcd5dde5c
Terrence <t.w.mcdonnell10@gmail.com>
2019-02-06 13:49:55 -0700

    [WIP] Change Script for future Sound Test implemention

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -5,12 +5,20 @@ using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    private bool playing = false;
 
     private FMOD.Studio.Bus master;
     private FMOD.Studio.Bus music;
     private FMOD.Studio.Bus SFX;
     private FMOD.Studio.Bus chopperSFX;
     private FMOD.Studio.Bus notChopperSFX;
+
+    [FMODUnity.EventRef]
+    private FMOD.Studio.EventInstance SFXTest;
+    [FMODUnity.EventRef]
+    private FMOD.Studio.EventInstance MusicTest;
+    [FMODUnity.EventRef]
+    private FMOD.Studio.EventInstance ChopperSFXTest;
 
     // Use this for initialization
     void Start()
@@ -25,31 +33,65 @@ public class AudioManager : MonoBehaviour
         SFX.setVolume(0.5f);
         chopperSFX.setVolume(0.05f);
         notChopperSFX.setVolume(1.0f);
+
+        SFXTest = FMODUnity.RuntimeManager.CreateInstance("event:/Missions/MissionObjectiveComplete");
+        MusicTest = FMODUnity.RuntimeManager.CreateInstance("event:/Music/MenuSong");
+        ChopperSFXTest = FMODUnity.RuntimeManager.CreateInstance("event:/Chopper/Accellerate");
+
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(SFXTest, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(MusicTest, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(ChopperSFXTest, GetComponent<Transform>(), GetComponent<Rigidbody>());
     }
 
     public void SetVolume(Slider volume)
     {
+        playing = true;
+        MusicTest.setVolume(volume.value);
         master.setVolume(volume.value);
+        TestSound(MusicTest);
     }
 
     public void SetMusicVolume(Slider volume)
     {
+        playing = true;
+        MusicTest.setVolume(volume.value);
         music.setVolume(volume.value);
+        TestSound(MusicTest);
     }
 
     public void SetSFXVolume(Slider volume)
     {
+        playing = true;
+        SFXTest.setVolume(volume.value);
         SFX.setVolume(volume.value);
+        TestSound(SFXTest);
     }
 
     public void SetChopperSFXVolume(Slider volume)
     {
+        playing = true;
+        ChopperSFXTest.setVolume(volume.value);
         chopperSFX.setVolume(volume.value);
+        TestSound(ChopperSFXTest);
     }
 
     public void SetNotChopperSFXVolume(Slider volume)
     {
+        playing = true;
+        SFXTest.setVolume(volume.value);
         notChopperSFX.setVolume(volume.value);
+        TestSound(SFXTest);
+    }
+
+    IEnumerator TestSound(FMOD.Studio.EventInstance instance)
+    {
+        while (playing)
+        {
+            instance.start();
+            yield return new WaitForSeconds(0.5f);
+            instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            playing = false;
+        }
     }
 
     // Update is called once per frame
