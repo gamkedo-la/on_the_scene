b28454c16446eac87d734ccdfbb6b59f82b20ec6
jeremyjackson89 <jeremy.jackson089@gmail.com>
2019-02-05 22:50:40 -0500

    Create Credits scene. If the player selects New Game, clear all saved data except for settings.

--- Assets/Scripts/MainMenu/MenuButtons.cs
+++ Assets/Scripts/MainMenu/MenuButtons.cs
@@ -8,6 +8,22 @@ public class MenuButtons : MonoBehaviour {
     public string sceneToLoad;
     public void ClickAction()
     {
+        if (transform.parent.gameObject.name == "New Game") {
+            ClearSavedMissionData();
+        }
         SceneManager.LoadScene(sceneToLoad);
+    }
+
+    void ClearSavedMissionData()
+    {
+        float masterVolume = PlayerPrefs.GetFloat("masterVolume", 1f);
+        float musicVolume = PlayerPrefs.GetFloat("musicVolume", 0.5f);
+        float sfxVolume = PlayerPrefs.GetFloat("sfxVolume", 0.5f);
+        
+        PlayerPrefs.DeleteAll();
+
+        PlayerPrefs.SetFloat("masterVolume", masterVolume);
+        PlayerPrefs.SetFloat("musicVolume", musicVolume);
+        PlayerPrefs.SetFloat("sfxVolume", sfxVolume);
     }
 }
